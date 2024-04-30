8bdee0294c913050b65d1165ac6df10ea0726975
Art Ivshin <boilroom@yandex.ru>
2024-04-30 14:38:54 +0500

    remove unused code

--- apps/panel/src/main.js
+++ apps/panel/src/main.js
@@ -7,11 +7,5 @@ import { InstallCodemirro } from "codemirror-editor-vue3"
 
 const app = createApp(App);
 
-app.config.errorHandler = (err, vm, info) => {
-  console.error("111 Error:", err);
-  console.error("111 Vue component:", vm);
-  console.error("111 Additional info:", info);
-};
-
 app.use(InstallCodemirro);
 app.mount("#app");
